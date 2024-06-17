83a65ae885163adef4fee698c9830a28eb2c9cd7
gitpremsagar <psagar172@gmail.com>
2024-06-17 15:12:00 +0530

    fix - Comment out multer config file codes

--- src/configs/multerConfig.ts
+++ src/configs/multerConfig.ts
@@ -1,27 +1,27 @@
-import multer from "multer";
-import path from "path";
+// import multer from "multer";
+// import path from "path";
 
-const storage = multer.diskStorage({
-  destination: "public/images/uploads/",
-  filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "public/images/uploads/",
+//   filename: function (req, file, cb) {
+//     cb(null, `${Date.now()}-${file.originalname}`);
+//   },
+// });
 
-const fileFilter = (req: any, file: any, cb: any) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
+// const fileFilter = (req: any, file: any, cb: any) => {
+//   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+//     cb(null, true);
+//   } else {
+//     cb(null, false);
+//   }
+// };
 
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 1024 * 1024 * 5, // 1MB
-  },
-});
+// const upload = multer({
+//   storage,
+//   fileFilter,
+//   limits: {
+//     fileSize: 1024 * 1024 * 5, // 1MB
+//   },
+// });
 
-export default upload;
+// export default upload;
